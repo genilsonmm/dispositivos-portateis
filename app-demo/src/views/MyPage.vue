b23276b666e33a2170c35b851ab39d63c5eb9a70
genilson.martins <genilson.martins@virtus.ufcg.edu.br>
2025-09-11 18:38:43 -0300

    inicial

---
+++ app-demo/src/views/MyPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>MyPage</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Inicio</ion-title>
+                </ion-toolbar>
+            </ion-header>
+
+            <h1>Minha Página</h1>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+
+import { IonPage, IonHeader, IonContent, IonToolbar, IonTitle } from '@ionic/vue';
+
+</script>
